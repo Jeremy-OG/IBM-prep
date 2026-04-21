9392ea27e218bbd65e73164d60ed9c1d813c4a30
Jeremy-OG <jeremyogene7@gmail.com>
2026-04-21 13:53:29 -0500

    I/O analyzer chart with refreshing built in

--- week1/disk_stats.py
+++ week1/disk_stats.py
@@ -11,4 +11,15 @@ def read_diskstats():
             stats[device] = (reads,writes)
     return stats
 prev = read_diskstats()
-print(prev)
+
+while True:
+    time.sleep(2)
+    print(f"{"DEVICE":<12}{"READS/S":<12}{"WRITES/S":<12}")
+    print("-"*36)
+    curr = read_diskstats()
+    for device in curr:
+        reads_sec = (curr[device][0] -prev[device][0])/2
+        writes_sec = (curr[device][1]-prev[device][1])/2
+        print(f"{device:<12}{reads_sec:<12.1f}{writes_sec:<12}")
+    prev = curr
+
