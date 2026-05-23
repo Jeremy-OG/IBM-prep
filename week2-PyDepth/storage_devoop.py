e0476acd205e8147a947a3c213d1d9d855b158aa
Jeremy-OG <jeremyogene7@gmail.com>
2026-05-22 23:42:38 -0500

    sync and async time comparison for local files

--- week2-PyDepth/storage_devoop.py
+++ week2-PyDepth/storage_devoop.py
@@ -3,8 +3,9 @@ import json
 from typing import Optional
 import asyncio
 import aiofiles
-
-
+import time
+from typing import Iterable
+devices =["device1.json","device2.json","device3.json","device4.json","device5.json"]
 @dataclass
 class DiskHealth:
     temperature: int
@@ -48,9 +49,29 @@ class StorageDevice:
         )
         
 
-path = StorageDevice('smart_sample.json')
+async def poll_all_devices(devices:list[str]) ->Iterable[tuple]:
+        coroutines = [StorageDevice(device).get_smart_health() for device in devices]
+        results = await asyncio.gather(*coroutines)
+        for result in  results:
+             print(result)
 
-result = asyncio.run(path.get_smart_health())
-print(result)
+
+#path = StorageDevice('smart_sample.json')
+
+
+
+# async version
+start = time.perf_counter()
+asyncio.run(poll_all_devices(devices))
+end = time.perf_counter()
+print(f"Async time: {end - start:.4f} seconds")
+
+# sync version
+start = time.perf_counter()
+for device in devices:
+    path = StorageDevice(device)
+    print(asyncio.run(path.get_smart_health()))
+end = time.perf_counter()
+print(f"Sync time: {end - start:.4f} seconds")
 
 
