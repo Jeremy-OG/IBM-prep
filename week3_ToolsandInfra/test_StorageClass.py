7ec5d2b6a248599bc1e8fe198d1a2a27b9552763
Jeremy-OG <jeremyogene7@gmail.com>
2026-05-24 16:05:08 -0500

    initialize necessary imports

---
+++ week3_ToolsandInfra/test_StorageClass.py
@@ -0,0 +1,9 @@
+from week2_PyDepth.storage_devoop import StorageDevice, DiskHealth
+from unittest.mock import patch
+import unittest
+
+
+class TestStorageclass(unittest.TestCase):
+
+    def test_capacity(self):
+        pass
