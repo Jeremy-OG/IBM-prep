0d0b607b0d2e27e7bf2dd83890abe3d0ecb7f064
Jeremy-OG <jeremyogene7@gmail.com>
2026-05-24 21:55:47 -0500

    capacity float unit test

---
+++ week3_ToolsandInfra/test_StorageDevice.py
@@ -0,0 +1,15 @@
+from week2_PyDepth.storage_devoop import StorageDevice, DiskHealth
+from unittest.mock import patch, mock_open
+import unittest
+
+
+class TestStorageclass(unittest.TestCase):
+
+
+
+
+    def test_capacity_returns_float(self):
+        with patch("builtins.open",mock_open(read_data="976773168")):
+            device = StorageDevice("sda")
+            result = device.capacity
+            assert isinstance(result,float)
